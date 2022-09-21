ada8086cc61b7ec0e6242f162184033d63e37af9
Troy Snowden <troybradsnowden@gmail.com>
2022-09-21 12:43:17 +0200

    feat(tbs-2): add simple home page

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const { app } = require("express");
+const express = require('express')
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 app.use(express.json({ limit: "30mb" }));
+
+// start app on port 8080
+app.listen(8080, function () {
+    console.log('Application Started')
+})
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
