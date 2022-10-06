80db2a7edac4e1965279fa0a8a0d2fe5fcc94467
Troy Snowden <troybradsnowden@gmail.com>
2022-10-06 20:12:21 +0100

    feat(tbs-7): connect with mongodb database

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config({path: __dirname + '/.env'})
 
 const app = express();
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
+    .then(() => {
+        console.log("Connection to database was successful");
+    })
+    .catch(error => {
+        console.log("An error occured connecting to database")
+    })
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
